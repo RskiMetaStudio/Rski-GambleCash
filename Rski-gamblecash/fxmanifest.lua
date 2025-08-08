63d77a1bd312bf6cd3709f936b38810093b539ff
Meta Studio's <liamcracknell2024@icloud.com>
2025-08-08 20:52:53 +0100

    Update fxmanifest.lua
    UPDATE FX MANIFEST.LUA

--- Rski-gamblecash/fxmanifest.lua
+++ Rski-gamblecash/fxmanifest.lua
@@ -1,12 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Rski.dev'
-description 'Gamble cash On FiveM'
+author 'Rski'
+description 'A Basic Free Npc Gamble Script For Fivem'
 version '1.0.0'
 
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
 
-dependencies { 'qb-core', 'ox_target' }
+dependencies { 'ox_target' }
